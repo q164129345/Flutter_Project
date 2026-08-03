8ec08a26b2951da0efc57de73e0a9e2d47720001
wallacezhang <wallace.zhang@fjdynamics.com>
2026-08-03 17:18:51 +0800

    完成章节”创建widget“

---
+++ widget_example1/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'game.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Tile('A', HitType.hit))),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Replace Container with widgets.
+    return Container(
+      width: 60, //
+      height: 60, //
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300), //
+        color: switch (hitType) {
+          HitType.hit => Colors.green, //
+          HitType.partial => Colors.yellow, //
+          HitType.miss => Colors.grey, //
+          _ => Colors.white, //
+        },
+      ),
+    );
+  }
+}
