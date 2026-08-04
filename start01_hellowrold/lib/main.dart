ecfa2f4b8281eed7589d6839c500c9300f3fd70e
wallacezhang <wallace.zhang@fjdynamics.com>
2026-08-04 21:28:45 +0800

    add two new project

---
+++ start01_hellowrold/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Flutter layout demo';
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text(appTitle)), //
+        body: const Center(child: Text('Hello, World!')),
+      ),
+    );
+  }
+}
