bd198917da9c8f07d321c8f16a9cdb1f4f5170a4
wallacezhang <wallace.zhang@fjdynamics.com>
2026-08-07 13:36:29 +0800

    new example

---
+++ scaffold02_actionbutton/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo', // 应用程序标题(UI上不显示它)
+      home: ScaffoldActionButton(), // 调用StatefulWidget对象ScaffoldActionButton
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class ScaffoldActionButton extends StatefulWidget {
+  const ScaffoldActionButton({super.key});
+
+  @override
+  State<ScaffoldActionButton> createState() => _ScaffoldActionButtonState();
+}
+
+class _ScaffoldActionButtonState extends State<ScaffoldActionButton> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Scaffold Action Button')),
+      body: Center(child: Text('You have pressed the button $_counter times.')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() {
+          _counter++;
+        }),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
