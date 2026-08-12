d960827f648834d3aa523ba6b83f4e68896f0440
wallacezhang <wallace.zhang@fjdynamics.com>
2026-08-12 11:36:18 +0800

    update project

--- gesture_detector/lib/main.dart
+++ gesture_detector/lib/main.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+  const MainApp({super.key}); // This widget is the root of your application.
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: GestureDetector(
+            // 点击事件
+            onTap: () {
+              setState(() {
+                counter++; // 点击时计数器加一
+              });
+            },
+
+            // 长按事件
+            onLongPress: () {
+              setState(() {
+                counter = 0; // 长按时计数器归零
+              });
+            },
+
+            // 双击事件
+            onDoubleTap: () {
+              setState(() {
+                counter--; // 双击时计数器减一
+              });
+            },
+
+            // 上下滑动事件
+            onPanUpdate: (details) {
+              setState(() {
+                counter += details.delta.dy > 0 ? 1 : -1; // 上下滑动时计数器加减
+              });
+            },
+
+            child: Text('Counter: $counter', style: TextStyle(fontSize: 24)),
+          ),
         ),
       ),
     );
