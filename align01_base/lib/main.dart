b2d27fd403712a44ba666710c04b4d99fbc1669f
wallacezhang <wallace.zhang@fjdynamics.com>
2026-08-14 15:26:28 +0800

    更新代码

--- align01_base/lib/main.dart
+++ align01_base/lib/main.dart
@@ -20,13 +20,13 @@ class MainApp extends StatelessWidget {
           width: double.infinity,
           height: double.infinity,
           decoration: BoxDecoration(color: Colors.yellow),
-          child: Column(
+          child: Row(
             // MainAxisAlignment.center
             // MainAxisAlignment.end
             // MainAxisAlignment.spaceAround
             // MainAxisAlignment.spaceBetween
             mainAxisAlignment: MainAxisAlignment.center, // 主轴居中
-            crossAxisAlignment: CrossAxisAlignment.end, // 交叉轴靠右
+            crossAxisAlignment: CrossAxisAlignment.center, // 交叉轴居中
             children: [
               Container(
                 width: 80, //
@@ -34,13 +34,13 @@ class MainApp extends StatelessWidget {
                 color: Colors.blue,
               ),
               Container(
-                margin: EdgeInsets.only(top: 10), // 上部外边距
+                margin: EdgeInsets.only(left: 10), // 左外边距
                 width: 80, //
                 height: 80,
                 color: Colors.blue,
               ),
               Container(
-                margin: EdgeInsets.only(top: 10), // 上部外边距
+                margin: EdgeInsets.only(left: 10), // 左外边距
                 width: 80, //
                 height: 80,
                 color: Colors.blue,
