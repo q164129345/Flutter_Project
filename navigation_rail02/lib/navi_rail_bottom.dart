284b9fbfd899fbbf2e3d96f454d947e1433364e7
wallacezhang <wallace.zhang@fjdynamics.com>
2026-08-26 16:56:25 +0800

    更新代码

--- navigation_rail02/lib/navi_rail_bottom.dart
+++ navigation_rail02/lib/navi_rail_bottom.dart
@@ -43,20 +43,17 @@ class _NaviRailBottomState extends State<NaviRailBottomState> {
       child: Padding(
         // Padding 位于 InkWell 外面，避免扩大水波纹区域。
         padding: const EdgeInsets.symmetric(vertical: 8),
-        child: Container(
+        child: SizedBox(
           width: 56, // 设置指示器宽度
           height: 32, // 设置指示器高度
-          clipBehavior: Clip.antiAlias, // 按圆角裁剪水波纹，防止波纹超出指示器
-          decoration: BoxDecoration(
-            color: indicatorColor, // 根据选中、按下或悬停状态显示背景色
-            borderRadius: BorderRadius.circular(20), // 设置胶囊形圆角
-          ),
           // 为什么这里需要套一层Material()? 因为InkWell需要被一个Material父类套住。然后，Container并不是一个Material组件
           // 比如Scafolld是一个Material组件。
           // Note: 其实InkWell发现上一层没办法提供Material表面时，会一层一层往上找，如果能找到的话，水纹效果就正常显示。
           // 所以，最保险就是在InkWell外面套一层Material()，这样就不需要依赖父类是否能提供Material表面。
           child: Material(
-            color: Colors.transparent, // 透明
+            color: indicatorColor, // 根据选中、按下或悬停状态显示背景色
+            borderRadius: BorderRadius.circular(20), // 设置胶囊形圆角
+            clipBehavior: Clip.antiAlias, // 按圆角裁剪水波纹，防止波纹超出指示器
             child: InkWell(
               onTap: widget.onTap,
 
@@ -76,7 +73,7 @@ class _NaviRailBottomState extends State<NaviRailBottomState> {
 
               //mouseCursor: SystemMouseCursors.click, // 鼠标悬停时显示可点击的手形光标
               borderRadius: BorderRadius.circular(20), // 将水波纹裁剪为胶囊形圆角
-              // 悬停和按下背景由 Container 根据状态重新构建后显示。 负责。
+              // 悬停和按下背景由 Material 根据状态重新构建后显示。 负责。
               hoverColor: Colors.transparent, // 透明
               highlightColor: Colors.transparent, // 透明
               splashColor: colorScheme.primary.withAlpha(
