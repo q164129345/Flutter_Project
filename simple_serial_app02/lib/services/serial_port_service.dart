94e81e972e9482d432cbfc846f97f7c372dfb9be
wallacezhang <wallace.zhang@fjdynamics.com>
2026-08-31 15:46:45 +0800

    增加接收功能

--- simple_serial_app02/lib/services/serial_port_service.dart
+++ simple_serial_app02/lib/services/serial_port_service.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
@@ -6,15 +7,28 @@ import 'package:flutter_libserialport/flutter_libserialport.dart';
 
 
 class SerialPortService {
+  /// 串口实例
   SerialPort? _port;
-
-  /// 当前串口是否已经打开
-  bool get isConnected => _port?.isOpen ?? false;
 
   /// 获取系统当前有效串口
   List<String> getAvailablePorts() {
     return SerialPort.availablePorts; // 通过系统接口枚举已存在串口名称
   }
+
+  /// 读串口的句柄
+  SerialPortReader? _reader;
+
+  /// 接收监听
+  StreamSubscription<String>? _readerSubscription;
+
+  /// 将接收的字符串消息发给UI
+  final StreamController<String> _receivedTextController = StreamController<String>.broadcast();
+
+  /// UI通过这个Stream 获取串口接收的数据
+  Stream<String> get receviedTextStream => _receivedTextController.stream;
+
+  /// 当前串口是否已经打开
+  bool get isConnected => _port?.isOpen ?? false;
 
   /// 打开串口
   void connect({
@@ -65,6 +79,9 @@ class SerialPortService {
 
       // 配置成功以后，才保存这个串口对象
       _port = port;
+
+      // 开始监听接收串口数据
+      _startReading();
     } catch (e) {
       try {
         if (port.isOpen) {
@@ -76,6 +93,37 @@ class SerialPortService {
 
       rethrow;
     }
+  }
+
+  /// 开始接收数据
+  void _startReading() {
+    final port = _port;
+
+    if (port == null) return;
+
+    // 创建异步串口读取器
+    _reader = SerialPortReader(port);
+
+    _readerSubscription = _reader!
+      .stream
+      .cast<List<int>>()
+      // Uint8List
+      //     ↓
+      // UTF-8 String
+      .transform(const Utf8Decoder(allowMalformed: true))
+      // String 数据流
+      //     ↓
+      // 按 \n / \r\n分割成一条条消息
+      .transform(const LineSplitter())
+      // 监控每一条完整的信息
+      .listen(
+        (message) {
+        _receivedTextController.add(message);
+        },
+        onError: (error) {
+          _receivedTextController.addError(error);
+        },
+      );
   }
 
   /// 发送字符串
@@ -96,8 +144,22 @@ class SerialPortService {
     return _port!.write(data);
   }
 
-  /// 关闭串口
+  /// 停止串口接收
+  void _stopReading() {
+    // 取消 Stream 监听
+    _readerSubscription?.cancel();
+    _readerSubscription = null;
+
+    // SerialPortReader 必须关闭
+    _reader?.close();
+    _reader = null;
+  }
+
+  /// 断开串口
   void disconnect() {
+    // 先停止接收
+    _stopReading();
+
     final port = _port;
 
     // 先解除当前对象引用
@@ -134,6 +196,13 @@ class SerialPortService {
       debugPrint('释放串口失败：$e');
     }
   }
+
+  /// 销毁整个Servie
+  void dispose() {
+    disconnect();
+    _receivedTextController.close();
+  }
+
 }
 
 
